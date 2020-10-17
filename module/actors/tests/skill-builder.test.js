ef1ed19a99a0d01eea160318391aa183c3bdff91
Evan Kirsch <etkirsch@gmail.com>
2020-10-17 12:00:02 -0400

    add Skill Builder

---
+++ module/actors/tests/skill-builder.test.js
@@ -0,0 +1,50 @@
+import SkillBuilder from '../skill-builder.js';
+
+describe('SkillBuilder', () => {
+  describe('constructor validation', () => {
+    it('raises exception with bad config input', () => {
+      expect(() => {
+        const builder = new SkillBuilder({});
+        builder.buildSkill('anyKey');
+      }).toThrowError(SkillBuilder.ImproperConfigFormat);
+    });
+  });
+
+  describe('buildSkill', () => {
+    it('raises exception with No Config when deleted', () => {
+      expect(() => {
+        const builder = new SkillBuilder({skillConfiguration: {}});
+        delete builder.config;
+        builder.buildSkill('anyKey');
+      }).toThrowError(SkillBuilder.NoConfigErrorText);
+    });
+
+    it('raises exception with No Config when deleted', () => {
+      const key = 'anyKey';
+      expect(() => {
+        const config = {
+          skillConfiguration: {
+            'aSkill': {},
+          },
+        };
+        const builder = new SkillBuilder(config);
+        builder.buildSkill(key);
+      }).toThrowError(SkillBuilder.noMatchingSkillKeyErrorText(key));
+    });
+
+    it('returns a built key when validation passes', () => {
+      const key = 'anyKey';
+      const skillName = 'skill name';
+      const config = {
+        skillConfiguration: {
+          [key]: {
+            name: skillName,
+          },
+        },
+      };
+      const builder = new SkillBuilder(config);
+      const builtSkill = builder.buildSkill(key);
+      expect(builtSkill.name).toBe(skillName);
+    });
+  });
+});
--- test.js
+++ test.js
@@ -1,15 +1,15 @@
-'use strict'
+'use strict';
 
-const jest = require('jest')
-require ('babel-polyfill')
+const jest = require('jest');
+require('babel-polyfill');
 
-process.env.BABEL_ENV = 'test'
-process.env.NODE_ENV = 'test'
+process.env.BABEL_ENV = 'test';
+process.env.NODE_ENV = 'test';
 
 process.on('unhandledRejection', (err) => {
-  throw err
-})
+  throw err;
+});
 
-let argv = process.argv.slice(2)
-argv.push("--verbose")
-jest.run(argv)
+let argv = process.argv.slice(2);
+argv.push("--verbose");
+jest.run(argv);
